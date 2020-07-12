64904451b57d751a6ca33e7fb492c2a37c28c89b
karta020500 <30350772+karta020500@users.noreply.github.com>
2020-07-12 10:02:44 +0800

    View Refactor

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -93,7 +93,7 @@
                 @foreach ($game['screenshots'] as $screenshot)
                     <div>
                         <a href="{{ $screenshot['huge'] }}">
-                            <img src="{{ $screenshot['big'] }}">
+                            <img src="{{ $screenshot['big'] }}" alt="screenshot" class="hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                     </div>
                 @endforeach
